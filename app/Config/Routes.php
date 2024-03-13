e930090a1a4753190d03135b238c5d04ed5f9daa
andretipolt <andretipoltlopes@gmail.com>
2024-03-13 10:03:51 -0300

    Creating form

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/',  'Main::index');
+$routes->post('/submit',  'Main::submit');
--- app/Views/home.php
+++ app/Views/home.php
@@ -1,7 +1,54 @@
 <?= $this->extend('layouts/main_layout')?>
 
 <?= $this->section('content')?>
-    <div>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus reiciendis at expedita obcaecati rerum ut, pariatur magnam odit eos? Magni et error totam consectetur mollitia pariatur minus itaque est odio!</p>
+    
+    <div class="container mt-5">
+
+        <div class="row">
+            <div class="col">
+
+            <?php form_open_multipart('/submit') ?>
+
+                <div class="mb-3">
+
+                    <label for="email" class="form-label">Email *</label>
+                    <input type="text" class="form-control" name="email" id="email" required>
+                </div>
+
+                <div class="mb-3">
+
+                    <label for="name" class="form-label">Nome</label>
+                    <input type="text" class="form-control" name="name" id="name">
+                </div>
+
+
+                <div class="mb-3">
+                    <label for="area" class="form-label">Area de Reclamação*</label>
+                    <select name="area" id="area" class="form-select" required>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <label for="complaint" class="form-label">Area texto para reclamação *</label>
+                    <textarea name="complaint" id="complaint" class="form-control"></textarea>
+                </div>
+
+                <div class="mb-3">
+                    <label for="files" class="form-label">Upload de ficheiros</label>
+                    <div class="d-flex flex-row justify-content-between gap-3">
+                        <input type="file" class="form-control" id="files1" name="files[]">
+                        <input type="file" class="form-control" id="files2" name="files[]">
+                        <input type="file" class="form-control" id="files3" name="files[]">
+                    </div>
+                </div>
+
+                <button class="btn btn-primary">Enviar</button>
+
+            <?php form_close()?>
+            </div>
+        </div>
     </div>
 <?= $this->endSection('content')?>
