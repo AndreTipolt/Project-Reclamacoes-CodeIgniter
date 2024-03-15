edb99d67ccb03dbdba3cbd466518703d61d930c3
andretipolt <andretipoltlopes@gmail.com>
2024-03-15 10:07:07 -0300

    Validate form

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -8,10 +8,34 @@ class Main extends BaseController
 {
     public function index()
     {
+        $data['validation_errors'] = session()->getFlashdata('errors');
+
+        if(!empty($errors)){
+            dd($errors);
+        }
+
         return view('home');
     }
 
     public function submit(){
+
+        $validation = $this->validate([
+
+            'email' => [
+                
+                'label' =>'Email',
+                'rules' => 'required','valid_email',
+                'errors' =>  [
+                    'required' => 'O campo {field} é obrigatório',
+                    'valid_email' => 'O campo {field} deve ser um email válido'
+                ]
+
+            ],
+        ]);
+
+        if(!$validation){
+            return redirect()->back()->withInput()->with('errors', $this->validator->getErrors());
+        }
 
         dd([
             $this->request->getPost(),
